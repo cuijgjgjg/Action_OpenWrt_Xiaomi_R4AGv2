bc920c997d1286738c06c44aeef0b9b3ffaf90ee
cuijgjgjg <125649801+cuijgjgjg@users.noreply.github.com>
2023-03-05 18:39:19 +0800

    Update light-part1_feeds.sh

--- LEDE/light-part1_feeds.sh
+++ LEDE/light-part1_feeds.sh
@@ -19,8 +19,8 @@ echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.defa
 # Add Theme package
 rm -rf package/lean/luci-theme-argon
 rm -rf package/lean/luci-app-argon-config
-sudo git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
-sudo git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
+#sudo git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/lean/luci-theme-argon
+#sudo git clone https://github.com/jerrykuku/luci-app-argon-config.git package/lean/luci-app-argon-config
 
 #rm -rf package/lean/luci-theme-argon_armygreen
 #sudo git clone https://github.com/XXKDB/luci-theme-argon_armygreen.git package/lean/luci-theme-argon_armygreen
